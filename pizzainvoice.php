0c132bf80a4851ce292fd469a9e370b44cf4c1e3
oansari1 <82239732+oansari1@users.noreply.github.com>
2022-06-05 23:46:54 -0700

    Assignment submission - needs work

---
+++ pizzainvoice.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>CID Assignment U08B</title>
+
+<!--
+
+CID Name Assignment U08B
+
+What objectives are you demonstrating?
+
+Receiving data via POST
+Assigning values to variables from incoming data
+Processing data and returning a page with proper markup
+
+-->
+
+<style>
+
+
+</style>
+
+</head>
+<body>
+<?php
+// Get pizza data sent by POST
+// isset() tests for data at the gateway, required on many servers
+// Dropdown list passes values ($size), Checkboxes are supposed to pass an array of checked values - I'm only getting the last item checked for now
+if(isset($_POST["customerName"])) { $name = $_POST["customerName"]; } else { $name = ""; }
+if(isset($_POST["size"])) { $size = $_POST["size"]; } else { $size = ""; }
+if(isset($_POST["topping"])) { $topping = array($_POST["topping"]); } else { $topping = array("Cheese"); }
+
+//Build a table for invoice - for some reason I keep getting an empty tag after my h2? It's not in the code but keeps showing up
+print "
+<body>
+<h1>O'Malley's Authentic Italian Pizzeria</h1>
+<h2>Customer name: $name</h2>
+<table><tr><th>Size:</th><td>$size</td><td>";
+if($size == "Small") {$total = 9.99; print $total;}
+if($size == "Medium") {$total = 11.99; print $total;}
+if($size == "Large") {$total = 468.38; print $total;}
+print "</td></tr><><th>Toppings:</th>";
+
+// Print topping if checked. Because only last item checked is ending up in the array, my total is not getting added properly
+if(in_array("Pepperoni", $topping)) {$total += 0.99; print "<tr><td>Pepperoni</td><td>$0.99</td></tr>";}
+if(in_array("Pumpkin", $topping)) {$total += 0.99; print "<tr><td>Pumpkin</td><td>$0.99</td></tr>";}
+if(in_array("Escargot", $topping)) {$total += 1.99; print "<tr><td>Escargot</td><td>$1.99</td></tr>";}
+if(in_array("Gerbils", $topping)) {$total += 8.99; print "<tr><td>Gerbils</td><td>$8.99</td></tr>";}
+if(in_array("Cheese", $topping)) {print "<tr><td>Just Cheese</td></tr>";}
+
+print "<tr><th>Total: </th><td>$ $total</td></tr></table></body>";
+?>
+</body>
+</html>
