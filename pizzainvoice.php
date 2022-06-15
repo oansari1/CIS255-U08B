8358dbdf2e04aeab22166e85bae0a91bf051460b
oansari1 <82239732+oansari1@users.noreply.github.com>
2022-06-15 13:26:43 -0700

    Fixed array functionality

--- pizzainvoice.php
+++ pizzainvoice.php
@@ -27,30 +27,39 @@ Processing data and returning a page with proper markup
 <?php
 // Get pizza data sent by POST
 // isset() tests for data at the gateway, required on many servers
-// Dropdown list passes values ($size), Checkboxes are supposed to pass an array of checked values - I'm only getting the last item checked for now
+// Dropdown list passes values ($size), Checkboxes pass an array of checked values
+
+
+
 if(isset($_POST["customerName"])) { $name = $_POST["customerName"]; } else { $name = ""; }
 if(isset($_POST["size"])) { $size = $_POST["size"]; } else { $size = ""; }
-if(isset($_POST["topping"])) { $topping = array($_POST["topping"]); } else { $topping = array("Cheese"); }
-
 //Build a table for invoice - for some reason I keep getting an empty tag after my h2? It's not in the code but keeps showing up
 print "
-<body>
+
 <h1>O'Malley's Authentic Italian Pizzeria</h1>
 <h2>Customer name: $name</h2>
 <table><tr><th>Size:</th><td>$size</td><td>";
+// Print the selected size and price of the size, add price to running total
 if($size == "Small") {$total = 9.99; print $total;}
 if($size == "Medium") {$total = 11.99; print $total;}
 if($size == "Large") {$total = 468.38; print $total;}
 print "</td></tr><><th>Toppings:</th>";
+// Print topping and price if checked, then add to the total
+if(isset($_POST["topping"])) {
+    if(in_array("Pepperoni", $_POST['topping'])) {$total += 0.99; print "<tr><td>Pepperoni</td><td>$0.99</td></tr>";}
+    if(in_array("Pumpkin", $_POST['topping'])) {$total += 0.99; print "<tr><td>Pumpkin</td><td>$0.99</td></tr>";}
+    if(in_array("Escargot", $_POST['topping'])) {$total += 1.99; print "<tr><td>Escargot</td><td>$1.99</td></tr>";}
+    if(in_array("Gerbils", $_POST['topping'])) {$total += 8.99; print "<tr><td>Gerbils</td><td>$8.99</td></tr>";}
+}
+else
+{print "<tr><td>Just Cheese</td></tr>";}
 
-// Print topping if checked. Because only last item checked is ending up in the array, my total is not getting added properly
-if(in_array("Pepperoni", $topping)) {$total += 0.99; print "<tr><td>Pepperoni</td><td>$0.99</td></tr>";}
-if(in_array("Pumpkin", $topping)) {$total += 0.99; print "<tr><td>Pumpkin</td><td>$0.99</td></tr>";}
-if(in_array("Escargot", $topping)) {$total += 1.99; print "<tr><td>Escargot</td><td>$1.99</td></tr>";}
-if(in_array("Gerbils", $topping)) {$total += 8.99; print "<tr><td>Gerbils</td><td>$8.99</td></tr>";}
-if(in_array("Cheese", $topping)) {print "<tr><td>Just Cheese</td></tr>";}
+print "<tr><th>Total: </th><td>$ $total</td></tr></table>";
 
-print "<tr><th>Total: </th><td>$ $total</td></tr></table></body>";
+
+
+
+
 ?>
 </body>
 </html>
